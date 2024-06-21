78ee7f547164b8cd0bd09d9224bd6606c77c8445
internessi <50959344+internessi@users.noreply.github.com>
2024-06-21 11:30:40 +0200

    Update INT-TTGO-TD-WIFI-ALARM.ino

--- INT-TTGO-TD-WIFI-ALARM.ino
+++ INT-TTGO-TD-WIFI-ALARM.ino
@@ -1,243 +1,99 @@
-#include <FS.h>
-#include <SPIFFS.h>
+/**************************************************************************
+  TTGO T-Display ST7789 OLED based on Adafruit example
+  https://github.com/adafruit/Adafruit-ST7735-Library/blob/master/examples/graphicstest/graphicstest.ino
+ **************************************************************************/
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
-#include "WiFi.h"
-#include <Wire.h>
-#include "esp_adc_cal.h"
 
+// pinouts from https://github.com/Xinyuan-LilyGO/TTGO-T-Display
+#define TFT_MOSI 19
+#define TFT_SCLK 18
+#define TFT_CS 5
+#define TFT_DC 16
+#define TFT_RST 23
+#define TFT_BL 4
 
+// constructor for data object named tft 
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
-
-#define FORMAT_SPIFFS_IF_FAILED true
-
-#define ADC_EN              14  //ADC_EN is the ADC detection enable port
-#define ADC_PIN             34
-
-
-
-
-
-int joystick;
-
-char buff[512];
-int vref = 1100;
-int btnCick = false;
-String output [6];
-
-void setup()
-{
-    Serial.begin(115200);
-    
-    espDelay(2000);
-    
-    display_text("Start",3,800);
-    
-    if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)){
-      display_text("SPIFFS Failed",3,800);
-      espDelay(1000);
-    if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)){
-        display_text("SPIFFS Broken",3,800);            
-      } else {
-        display_text("SPIFFS now OK",3,800); 
-      }
-    } else {
-      display_text("SPIFFS OK",3,800);    
-    }
-
-    if(!SPIFFS.exists("/ap.csv")){
-      writeFile(SPIFFS, "/ap.csv", "ID;NAME\r\n");
-      display_text("AP Init",3,800); 
-    } else {
-      display_text("AP OK",3,800);    
-    }
-
-    if(!SPIFFS.exists("/aps.csv")){
-      writeFile(SPIFFS, "/aps.csv", "ID;NAME\r\n");
-      display_text("Suche Init",3,800); 
-    } else {
-      display_text("Suche OK",3,800);    
-    }
-
-  //  read_spiffs_to_TFT(SPIFFS, "/aps.csv");
-
-    
-    pinMode(ADC_EN, OUTPUT);
-    digitalWrite(ADC_EN, HIGH);
-
-
-    esp_adc_cal_characteristics_t adc_chars;
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
-    //Check type of calibration value used to characterize ADC
-    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
-        Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
-        vref = adc_chars.vref;
-    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
-        Serial.printf("Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
-    } else {
-        Serial.println("Default Vref: 1100mV");
-    }
-  
-    output[0] = "ABCDEFGHIJKLMNOPQRS"; 
-    output[1] = "ZYXWVUTSRQPONMLKJIH"; 
-    output[2] = "abcdefghijklmnopqrs"; 
-    output[3] = "zyxwvutsrqponmlkjih"; 
-    output[4] = "1234567890!§$%&=?*+"; 
-    output[5] = "ABCDEFGHIJKLMNOPQRS"; 
-        Serial.println(" ");
-                 Serial.println(" ");
+void setup(void) {
+  Serial.begin(115200); Serial.print(F("Hello! ST77xx TFT Test"));
+  pinMode(TFT_BL, OUTPUT);      // TTGO T-Display enable Backlight pin 4
+  digitalWrite(TFT_BL, HIGH);   // T-Display turn on Backlight
+  tft.init(135, 240);           // Initialize ST7789 240x135
+  Serial.println(F("Initialized"));
 }
 
-void loop()
-{
-    
-    //showVoltage();
+void loop() {
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setRotation(3);
+  time = millis() - time;
 
-    wifi_scan();
-    delay(29000);  
+  Serial.println(time, DEC);
+  delay(500);
+
+  // large block of text
+  tft.fillScreen(ST77XX_BLACK);
+  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
+  delay(3000);
+
+  // tft print function!
+  tftPrintTest();
+  delay(4000);
+
+  Serial.println("repeat");
+  delay(1000);
 
 }
 
-// START read_spiffs_to_TFT()
-void read_spiffs_to_TFT(fs::FS &fs, const char * path){
-    int i;
-    Serial.println("Start");
-    File file = fs.open(path);
-    if(!file || file.isDirectory()){
-        Serial.println("- failed");
-        return;
-    }
-    while(file.available()){
-    i= file.readBytesUntil(';', buff, sizeof(buff));
-      buff[i] = 0;
-      display_text(buff,2,400);     
-      i= file.readBytesUntil('\n', buff, sizeof(buff));
-      buff[i] = 0;
-      display_text(buff,2,400);  
-    }
-    Serial.println(" ");
-} // END read_spiffs_to_sram()
 
-// START  display_text()
-void display_text(String text, int tsize, int ttime) {
-  Serial.println(text);
-
-
-} // END  display_text()
-
-//! Long time delay, it is recommended to use shallow sleep, which can effectively reduce the current consumption
-void espDelay(int ms)
-{
-    esp_sleep_enable_timer_wakeup(ms * 1000);
-    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
-    esp_light_sleep_start();
+void testdrawtext(char *text, uint16_t color) {
+  tft.setTextSize(2);
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
 }
 
-void showVoltage()
-{
-    static uint64_t timeStamp = 0;
-    if (millis() - timeStamp > 1000) {
-        timeStamp = millis();
-        uint16_t v = analogRead(ADC_PIN);
-        float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
-        String voltage = "Voltage :" + String(battery_voltage) + "V";
-        Serial.println(voltage);
-    }
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_GREEN);
+  float p = 3.1415926;    // do not delete
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(" seconds.");
 }
 
-
-
-
-void wifi_scan()
-{
-    delay(1000);
-    Serial.println("Start");
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(500);
-
-    int16_t n = WiFi.scanNetworks();
-    if (n == 0) {
-        Serial.println("keine Netzwerke");
-    } else {
-        // Serial.printf("gefunden %d net\n", n);
-        for (int i = 0; i < n; ++i) {
-            String router, shelly;
-            router = WiFi.SSID(i).c_str();
-        
-            
-            if (router.length() == 19){
-              
-              shelly = router.substring(0,13);
-              
-              
-              if(shelly == "shellyplug-s-"){
-
-                Serial.print("Shelly: ");
-                shelly = router.substring(13,19);
-                Serial.println(shelly);
-                }
-              }
-
-
-
-              
-            // Serial.println(router);
-            // sprintf(buff,
-            //        "[%d]:%s(%d)",
-            //        i + 1,
-            //        router.c_str(),
-            //        WiFi.RSSI(i));
-            //Serial.println(buff);
-            //router = String(i + 1) + ";" + String(WiFi.SSID(i).c_str()) + "\r\n";     
-            //appendFile(SPIFFS, "/aps.csv", router);
-        }
-    }
-    WiFi.mode(WIFI_OFF);
-    Serial.println(" ");
-
-    
-}
-
-
-
-void writeFile(fs::FS &fs, const char * path, const char * message){
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("- failed to open file for writing");
-        return;
-    }
-    if(!file.print(message)){
-        Serial.println("- writeFile failed");
-    }
-}
-
-void readFile(fs::FS &fs, const char * path){
-    File file = fs.open(path);
-    if(!file || file.isDirectory()){
-        Serial.println("- failed to open file for reading");
-        return;
-    }
-
-    Serial.println("- read from file:");
-    while(file.available()){
-        Serial.write(file.read());
-    }
-}
-
-void deleteFile(fs::FS &fs, const char * path){
-    if(!fs.remove(path)){
-        Serial.println("- delete failed");
-    }
-}
-
-void appendFile(fs::FS &fs, const char * path, String text){
-    const char * message = text.c_str();
-    File file = fs.open(path, FILE_APPEND);
-    if(!file){
-        Serial.println("- failed to open file for appending");
-        return;
-    }
-    if(!file.print(message)){
-        Serial.println("- append failed");
-    }
-}
